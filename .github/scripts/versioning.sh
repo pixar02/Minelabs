e8fc136883786d6e77d914db7663c477e547f546
pixar02 <pixar.pf@gmail.com>
2023-11-20 22:59:10 +0100

    Update versioning.sh

--- .github/scripts/versioning.sh
+++ .github/scripts/versioning.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-base_version=$1
-merged_version=$2
+main_version=$1
+merge_version=$2
 
-if [[ "$base_version" == "$merged_version" ]]; then
-  new_version=$((merged_version + 1))
-  echo "Updating version to $new_version"
-  sed -i "s/version=$merged_version/version=$new_version/" gradle.properties
-else
-  echo "Merged version is greater, keeping it."
+IFS='.' read -ra mainV <<< "$main_version"
+IFS='.' read -ra mergeV <<< "$merge_version"
+
+check() {
+    for ((i=2; i>=0; i--)); do
+        base="${mainV[$i]}"
+        merge="${mergeV[$i]}"
+        if ((merge > base)); then
+            return 0
+        fi
+    done
+    return 1
+}
+
+if [ $(check) == 1 ]; then
+  v=$((mainV[2] + 1))
+  builder="${mainV[0]}.${mainV[1]}.$v"
+  echo "auto: "
+  echo "$builder"
+  sed -i "s/mod_version=$main_version/mod_version=$builder/" gradle.properties
+else 
+    echo "merge"
+    echo "$merge_version" 
 fi
+
