417bec14575f3e67d570c9eef664f20446582f81
pixar02 <pixar.pf@gmail.com>
2023-11-23 18:07:24 +0100

    Update versioning.sh

--- .github/scripts/versioning.sh
+++ .github/scripts/versioning.sh
@@ -1,36 +1,18 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <main_version> <merge_version>"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <version>"
     exit 1
 fi
 
 main_version=$1
-merge_version=$2
 
-echo "main version: $main_version"
-echo "branch version: $merge_version"
+echo "version: $main_version"
 
 IFS='.' read -ra mainV <<< "$main_version"
-IFS='.' read -ra mergeV <<< "$merge_version"
 
-function check() {
-    for ((i=0; i<=2; i++)); do
-        if (( ${mergeV[$i]} > ${mainV[$i]} )); then
-          echo "MERGE IS BIGGER" 
-          return 0
-        fi
-    done
-    return 1
-}
+v=$((mainV[2] + 1))
+builder="${mainV[0]}.${mainV[1]}.$v"
+echo "$builder"
+sed -i "s/mod_version=$main_version/mod_version=$builder/" gradle.properties
 
-if check; then
-  v=$((mainV[2] + 1))
-  builder="${mainV[0]}.${mainV[1]}.$v"
-  echo "auto: "
-  echo "$builder"
-  sed -i "s/mod_version=$main_version/mod_version=$builder/" gradle.properties
-else
-    echo "merge"
-    echo "$merge_version"
-fi
