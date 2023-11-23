987d7dbee2dda91f09cdff623078d9dd16b415bb
pixar02 <pixar.pf@gmail.com>
2023-11-23 18:14:45 +0100

    auto v test (gamerules)

--- src/main/java/be/minelabs/world/MinelabsGameRules.java
+++ src/main/java/be/minelabs/world/MinelabsGameRules.java
@@ -1,6 +1,12 @@
 package be.minelabs.world;
 
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.world.GameRules;
+
 public class MinelabsGameRules {
+    public static final GameRules.Key<GameRules.BooleanRule> RANDOM_QUANTUM_DROPS = GameRuleRegistry.register(
+            "randomQuantumDrops", GameRules.Category.DROPS, GameRuleFactory.createBooleanRule(true));
 
     public static void onInitialize() {
 
